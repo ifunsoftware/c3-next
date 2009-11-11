fecbde6006f093d7bf67e253c84c3a9293148f54
mikhail.malygin <mikhail.malygin@8a917c1c-56ba-11de-8d7d-01d876f1962d>
2009-11-11 20:22:08 +0000

    [Change] Remove dependency from c3-platform
    git-svn-id: https://c3-system.googlecode.com/svn/c3-next/trunk@280 8a917c1c-56ba-11de-8d7d-01d876f1962d

---
+++ c3-client/src/org/aphreet/c3/platform/access/rmi/PlatformRmiAccessService.scala
@@ -0,0 +1,14 @@
+package org.aphreet.c3.platform.access.rmi
+
+import java.util.HashMap
+
+trait PlatformRmiAccessService {
+
+  def add(metadata:HashMap[String, String], file:String):String
+  
+  def get(ra:String):Array[Byte]
+  
+  def getMetadata(ra:String):HashMap[String, String]
+  
+  def getResourceAsString(ra:String):String
+}
---
+++ c3-client/src/org/aphreet/c3/platform/management/rmi/StorageDescription.scala
@@ -0,0 +1,8 @@
+package org.aphreet.c3.platform.management.rmi
+
+class StorageDescription(val id:String, val storageType:String, val contentType:String, val path:String, val mode:String) extends java.io.Serializable{
+  
+  override def toString:String = {
+    "[" + id + " " + storageType + " " + contentType + "]"
+  }
+}
