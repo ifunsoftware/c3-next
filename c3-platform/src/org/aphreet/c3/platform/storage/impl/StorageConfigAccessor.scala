74885f28288a7af0a4fe289a379f7b18d179360c
mikhail.malygin <mikhail.malygin@8a917c1c-56ba-11de-8d7d-01d876f1962d>
2010-01-24 17:42:05 +0000

    [Add] file from previous commit
    git-svn-id: https://c3-system.googlecode.com/svn/c3-next/trunk@356 8a917c1c-56ba-11de-8d7d-01d876f1962d

---
+++ c3-platform/src/org/aphreet/c3/platform/storage/impl/StorageConfigAccessor.scala
@@ -0,0 +1,109 @@
+package org.aphreet.c3.platform.storage.impl
+
+import java.io.{File, StringWriter}
+import java.util.{List => JList}
+
+import scala.collection.jcl.{LinkedList, Conversions, Set, HashMap}
+
+import org.aphreet.c3.platform.common.{Path, JSONFormatter}
+import org.aphreet.c3.platform.config.accessor._
+import org.aphreet.c3.platform.storage.{StorageParams, StorageMode, StorageModeParser}
+
+import com.springsource.json.parser.{Node, MapNode, ListNode, AntlrJSONParser, ScalarNode}
+import com.springsource.json.writer.JSONWriterImpl;
+
+import org.springframework.stereotype.Component
+
+@Component
+class StorageConfigAccessor extends ConfigAccessor[List[StorageParams]]{
+
+  val STORAGE_CONFIG = "c3-storage-config.json"
+  
+  def loadConfig(configDir:File):List[StorageParams] = {
+    val configFile = new File(configDir, STORAGE_CONFIG)
+    
+    if(configFile.exists){
+      val node = new AntlrJSONParser().parse(configFile)
+      val storageArray = node.asInstanceOf[MapNode].getNode("storages").asInstanceOf[ListNode].getNodes.toArray
+      
+      var list:List[StorageParams] = List()
+      
+      for(st <- storageArray){
+    	val storage = st.asInstanceOf[MapNode]
+    	 
+    	val ids = Conversions.convertList(storage.getNode("ids").asInstanceOf[ListNode].getNodes.asInstanceOf[JList[ScalarNode]])
+    	  
+        val idArray = for(node <- ids)
+        	  yield node.getValue.toString  	                                                                              
+        
+        val storageModeName = storage.getNode("mode").asInstanceOf[ScalarNode].getValue.toString
+        
+        var storageModeMessage = ""
+        
+        val storageModeMessageNode = storage.getNode("modemsg")
+        
+        
+        if(storageModeMessageNode != null){
+          storageModeMessage = storageModeMessageNode.asInstanceOf[ScalarNode].getValue.toString
+        }
+        
+        val storageMode = StorageModeParser.valueOf(storageModeName, storageModeMessage)
+        
+        
+    	list = list ::: List( 
+    	  new StorageParams(
+    	    storage.getNode("id").asInstanceOf[ScalarNode].getValue.toString,
+    	    List.fromIterator(idArray.elements),
+    		new Path(storage.getNode("path").asInstanceOf[ScalarNode].getValue.toString),
+    		storage.getNode("type").asInstanceOf[ScalarNode].getValue.toString,
+    		storageMode
+         ))
+      }
+      
+      
+      list
+    }else{
+      List()
+    }
+  }
+  
+  def storeConfig(params:List[StorageParams], configDir:File) = {
+    this.synchronized{
+    	
+      
+	    val swriter = new StringWriter()
+	    try{
+		    val writer = new JSONWriterImpl(swriter)
+		    
+		    writer.`object`.key("storages").array
+		    
+		    
+		    for(storage <- params){
+		      writer.`object`
+		        .key("id").value(storage.id)
+		        .key("path").value(storage.path)
+		        .key("type").value(storage.storageType)
+		        .key("mode").value(storage.mode.name)
+		        .key("modemsg").value(storage.mode.message)
+		        .key("ids").array
+		        for(id <- storage.secIds)
+		          writer.value(id)
+		        writer.endArray
+		        
+		      writer.endObject
+		    }
+		    writer.endArray
+		    writer.endObject
+		    
+		    swriter.flush
+      
+		    val result = JSONFormatter.format(swriter.toString)
+		    
+		    writeToFile(result, new File(configDir, STORAGE_CONFIG))
+      
+	    }finally{
+	    	swriter.close
+	    }
+    }
+  }
+}
