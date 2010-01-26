55d9e760f463fde5e1a7b06a0db649dfbf7f1f01
mikhail.malygin <mikhail.malygin@8a917c1c-56ba-11de-8d7d-01d876f1962d>
2010-01-26 22:21:28 +0000

    [Add] Some unit tests
    git-svn-id: https://c3-system.googlecode.com/svn/c3-next/trunk@361 8a917c1c-56ba-11de-8d7d-01d876f1962d

---
+++ c3-platform/src-test/org/aphreet/c3/platform/test/integration/config/MimeTypeConfigTest.scala
@@ -0,0 +1,29 @@
+package org.aphreet.c3.platform.test.integration.config
+
+import scala.collection.mutable.HashMap
+
+import org.aphreet.c3.platform.storage.dispatcher.selector.mime.{MimeTypeConfigAccessor, MimeConfigEntry}
+
+import junit.framework.Assert._
+
+class MimeTypeConfigTest extends AbstractTestWithFileSystem{
+
+  def testConfigPersistence = {
+    
+    val configAccessor = new MimeTypeConfigAccessor
+    
+    val config = List(
+    	MimeConfigEntry("*/*", "PureBDBStorage", false),
+    	MimeConfigEntry("image/*", "FileBDBStorage", true),
+    	MimeConfigEntry("image/png", "PureBDBStorage", true)
+    )
+     
+    configAccessor.storeConfig(config, testDir)
+    
+    
+    val readConfig = configAccessor.loadConfig(testDir)
+    
+    assertEquals(config, readConfig)
+    
+  }
+}
---
+++ c3-platform/src-test/org/aphreet/c3/platform/test/integration/storage/MimeTypeStorageSelectorTest.scala
@@ -0,0 +1,38 @@
+package org.aphreet.c3.platform.test.integration.storage
+
+import eu.medsea.mimeutil.MimeType
+
+import org.aphreet.c3.platform.storage.dispatcher.selector.mime._
+import org.aphreet.c3.platform.config.PlatformConfigManager
+
+import junit.framework.Assert._
+
+class MimeTypeStorageSelectorTest extends AbstractTestWithFileSystem{
+
+  def testConfigPersistence = {
+    
+    val configAccessor = new MimeTypeConfigAccessor
+    
+    val config = List(
+    	MimeConfigEntry("*/*", "PureBDBStorage", false),
+    	MimeConfigEntry("image/*", "FileBDBStorage", true),
+    	MimeConfigEntry("image/png", "PureBDBStorage", true)
+    )
+     
+    configAccessor.storeConfig(config, testDir)
+   
+    val configManager = new PlatformConfigManager
+    configManager.configDir = testDir
+    
+    configAccessor.setConfigManager(configManager)
+    
+    val selector = new MimeTypeStorageSelector
+    selector.setConfigAccessor(configAccessor)
+    selector.init
+    
+    assertEquals(MimeConfigEntry("image/png", "PureBDBStorage", true),selector.storageForType(new MimeType("image/png")))
+    assertEquals(MimeConfigEntry("image/*", "FileBDBStorage", true),selector.storageForType(new MimeType("image/jpeg")))
+    assertEquals(MimeConfigEntry("*/*", "PureBDBStorage", false),selector.storageForType(new MimeType("application/pdf")))
+    
+  }
+}
