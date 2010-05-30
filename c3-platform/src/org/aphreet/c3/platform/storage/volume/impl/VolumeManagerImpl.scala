7e24e579c1b83e4539d2fa5dd28ac65e13d34f47
mikhail.malygin <mikhail.malygin@8a917c1c-56ba-11de-8d7d-01d876f1962d>
2010-05-30 15:18:53 +0000

    [Change] Some code style changes
    git-svn-id: https://c3-system.googlecode.com/svn/c3-next/trunk@453 8a917c1c-56ba-11de-8d7d-01d876f1962d

--- c3-platform/src/org/aphreet/c3/platform/storage/volume/impl/VolumeManagerImpl.scala
+++ c3-platform/src/org/aphreet/c3/platform/storage/volume/impl/VolumeManagerImpl.scala
@@ -1,9 +1,39 @@
+/**
+ * Copyright (c) 2010, Mikhail Malygin
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the IFMO nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.aphreet.c3.platform.storage.volume.impl
 
 import org.springframework.stereotype.Component
 import org.springframework.beans.factory.annotation.Autowired
 
-import javax.annotation.{PostConstruct, PreDestroy}
+import javax.annotation.{PostConstruct}
 
 import org.apache.commons.logging.LogFactory
 
@@ -37,21 +67,21 @@ class VolumeManagerImpl extends VolumeManager with SPlatformPropertyListener{
   }
 
   def register(storage:Storage) = {
-	val volume = volumeForPath(storage.path.toString)
+    val volume = volumeForPath(storage.path.toString)
 
-	if(volume != null){
-		volume.storages + storage
-		storage.volume = volume
-	}else
-		throw new StorageException("Can't find volume for path: " + storage.path.toString)
+    if(volume != null){
+      volume.storages + storage
+      storage.volume = volume
+    }else
+      throw new StorageException("Can't find volume for path: " + storage.path.toString)
   }
 
   def unregister(storage:Storage) = {
-	val volume = volumeForPath(storage.path.toString)
-	if(volume != null)
-		volume.storages - storage
-	else
-		throw new StorageException("Can't find volume for path: " + storage.path.toString)
+    val volume = volumeForPath(storage.path.toString)
+    if(volume != null)
+      volume.storages - storage
+    else
+      throw new StorageException("Can't find volume for path: " + storage.path.toString)
   }
 
   private def volumeForPath(path:String):Volume = {
@@ -62,8 +92,8 @@ class VolumeManagerImpl extends VolumeManager with SPlatformPropertyListener{
         if(foundVolume == null)
           foundVolume = volume
         else
-          if(foundVolume.mountPoint.length < volume.mountPoint.length)
-        	foundVolume = volume
+        if(foundVolume.mountPoint.length < volume.mountPoint.length)
+          foundVolume = volume
 
     foundVolume
 
