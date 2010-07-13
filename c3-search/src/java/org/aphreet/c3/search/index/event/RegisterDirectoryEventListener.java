5856461fe0c23ff135a24589a241cc10f9c9223f
mikhail.malygin <mikhail.malygin@8a917c1c-56ba-11de-8d7d-01d876f1962d>
2010-07-13 07:08:37 +0000

    Removing search
    git-svn-id: https://c3-system.googlecode.com/svn/c3-next/trunk@501 8a917c1c-56ba-11de-8d7d-01d876f1962d

--- c3-search/src/java/org/aphreet/c3/search/index/event/RegisterDirectoryEventListener.java
+++
@@ -1,9 +0,0 @@
-package org.aphreet.c3.search.index.event;
-
-public interface RegisterDirectoryEventListener {
-	
-	void addDirectoryEventListener(DirectoryEventListener listener);
-
-	void removeDirectoryEventListener(DirectoryEventListener listener);
-
-}
