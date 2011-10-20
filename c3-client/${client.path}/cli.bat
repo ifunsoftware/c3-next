4e6e826514abb8ae683746a414c5e050d5774ffa
Mikhail Malygin <mikhail.malygin@gmail.com>
2011-10-21 01:23:46 +0400

    [Remove] temp directories

--- c3-client/${client.path}/cli.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set CLASSPATH=*;../target/*
-java Shell -t ws %*
--- c3-client/${client.path}/cli_https.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export CLASSPATH=*:../target/*
-java -Djavax.net.ssl.trustStore=c3.keystore Shell -t ws -ignoreSSLHostname "$@"
