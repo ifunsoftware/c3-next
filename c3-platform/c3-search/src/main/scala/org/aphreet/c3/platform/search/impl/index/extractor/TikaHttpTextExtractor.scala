72b09cf283a234f099f4bb697068ab8d92d33aa7
Mikhail Malygin <mikhail.malygin@gmail.com>
2013-02-05 03:32:00 +0400

    [Fixed] max indexable content size, refactored

--- c3-platform/c3-search/src/main/scala/org/aphreet/c3/platform/search/impl/index/extractor/TikaHttpTextExtractor.scala
+++ c3-platform/c3-search/src/main/scala/org/aphreet/c3/platform/search/impl/index/extractor/TikaHttpTextExtractor.scala
@@ -66,12 +66,12 @@ class TikaHttpTextExtractor(val tikaHostName: String) extends TextExtractor {
 
               Files.copy(is, path, StandardCopyOption.REPLACE_EXISTING)
 
-              if(!isSmallEnough(path.toFile.length())){
+              if(isSmallEnough(path.toFile.length())){
+                Some(new TikaExtractedDocument(path.toFile, headersMap))
+              }else{
                 log.info("Content of the resource " + address + " was skipped due to too long length: " + path.toFile.length())
                 Files.deleteIfExists(path)
                 None
-              }else{
-                Some(new TikaExtractedDocument(path.toFile, headersMap))
               }
             }
             case _ => None
