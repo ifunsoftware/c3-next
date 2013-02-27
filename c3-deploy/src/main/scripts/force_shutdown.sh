c1e661cff72671b75024ac420927b3b51921fad4
Mikhail Malygin <mikhail.malygin@gmail.com>
2013-02-28 01:08:37 +0400

    [Fixed] force_shutdown script did not work on node1

--- c3-deploy/src/main/scripts/force_shutdown.sh
+++ c3-deploy/src/main/scripts/force_shutdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VIRGO_PID=$(ps aux | grep java | grep jmxremote | cut -d ' ' -f 3)
+VIRGO_PID=$(ps aux | grep java | grep jmxremote | sed -e 's/[ ][ ]*/ /g' | cut -d ' ' -f 2)
 
 if [ "$VIRGO_PID" -eq "$VIRGO_PID" ] 2>/dev/null ; then echo "Found virgo process. Killing $VIRGO_PID" && kill $VIRGO_PID; fi
 
