06cb9f8a603b7d0947a5792ae36e7d8e9bbc26f9
Mikhail Malygin <mikhail.malygin@gmail.com>
2013-03-18 21:11:04 +0400

    [Add] kill -9 to the force_shutdown script

--- c3-deploy/src/main/scripts/force_shutdown.sh
+++ c3-deploy/src/main/scripts/force_shutdown.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-VIRGO_PID=$(ps aux | grep java | grep jmxremote | sed -e 's/[ ][ ]*/ /g' | cut -d ' ' -f 2)
+function find_virgo {
+    local virgo_pid=$(ps aux | grep java | grep jmxremote | sed -e 's/[ ][ ]*/ /g' | cut -d ' ' -f 2);
+    echo ${virgo_pid}
+}
 
-if [ "$VIRGO_PID" -eq "$VIRGO_PID" ] 2>/dev/null ; then echo "Found virgo process. Killing $VIRGO_PID" && kill $VIRGO_PID; fi
+VIRGO_PID=$(find_virgo)
+
+if [ "$VIRGO_PID" -eq "$VIRGO_PID" ] 2>/dev/null ; then
+    echo "Found virgo process. Killing $VIRGO_PID" && kill $VIRGO_PID;
+    sleep 5
+
+    VIRGO_PID=$(find_virgo)
+    if [ "$VIRGO_PID" -eq "$VIRGO_PID" ] 2>/dev/null ; then
+        echo "Virgo is still alive. Terminating" && kill -9 $VIRGO_PID
+    fi;
+fi
 
