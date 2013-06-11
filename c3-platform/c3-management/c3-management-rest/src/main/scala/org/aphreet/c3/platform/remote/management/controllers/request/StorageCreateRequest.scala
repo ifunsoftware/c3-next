d35dcec080bec24c530ac22a8d9f373d5142f5c6
Mikhail Malygin <mikhail.malygin@gmail.com>
2013-06-11 22:29:08 +0400

    [Add] authentication [Add] Create storage request

--- c3-platform/c3-management/c3-management-rest/src/main/scala/org/aphreet/c3/platform/remote/management/controllers/request/StorageCreateRequest.scala
+++ c3-platform/c3-management/c3-management-rest/src/main/scala/org/aphreet/c3/platform/remote/management/controllers/request/StorageCreateRequest.scala
@@ -2,9 +2,8 @@ package org.aphreet.c3.platform.remote.management.controllers.request
 
 import scala.beans.BeanProperty
 
-class StorageCreateRequest(
-    @BeanProperty var storageType: String,
-    @BeanProperty var path: String){
+case class StorageCreateRequest(@BeanProperty var storageType: String,
+                           @BeanProperty var path: String){
 
-    def this() = this(null, null)
-  }
+  def this() = this(null, null)
+}
