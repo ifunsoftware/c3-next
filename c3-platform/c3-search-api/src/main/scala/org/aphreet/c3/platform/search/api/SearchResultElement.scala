0710345b4e1662a1c19381a3ab1c58739b409566
jingerbread <qwadrillion@gmail.com>
2014-01-05 01:09:19 +0400

    [fixed] printing search results

--- c3-platform/c3-search-api/src/main/scala/org/aphreet/c3/platform/search/api/SearchResultElement.scala
+++ c3-platform/c3-search-api/src/main/scala/org/aphreet/c3/platform/search/api/SearchResultElement.scala
@@ -9,6 +9,14 @@ case class SearchResultElement(address:String,
                                    def setPath(newPath:String):SearchResultElement =
                                      SearchResultElement(address, newPath, score, fragments)
 
+                                   override def toString = {
+                                     "SearchResultElement(address: %s, path: %s score: %f fragments:[ %s ".format( address, path, score, fragments.mkString(","))
+                                   }
+
                                  }
 
-case class SearchResultFragment(field:String, foundStrings:Array[String])
+case class SearchResultFragment(field:String, foundStrings:Array[String])  {
+  override def toString = {
+    "SearchResultFragment(field: %s, foundStrings:[ %s ]".format( field, foundStrings.mkString(","))
+  }
+}
